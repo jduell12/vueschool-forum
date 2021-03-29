a878cc4211f97b68f45d06d6540aa7a795458e12
Jessica <duellje@oregonstate.edu>
2021-03-28 22:23:10 -0400

    added routes to the application

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Home from "./components/Home";
+import ThreadShow from "./components/ThreadShow";
 import { createRouter, createWebHistory } from "vue-router";
 
 //define routes which map to a component
-const routes = [{ path: "/", name: "Home", component: Home }];
+const routes = [
+  { path: "/", name: "Home", component: Home },
+  {
+    path: "/thread/:id",
+    name: "ThreadShow",
+    component: ThreadShow,
+    props: true
+  }
+];
 
 //create router instance
 const router = createRouter({
